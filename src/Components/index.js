02874f764fa32ef1493a7ae9f44fcdfb6687e873
Akmanbu <m.anbalagan9842@gmail.com>
2022-12-23 18:01:45 +0530

    Tamil-Tube

---
+++ src/Components/index.js
@@ -0,0 +1,12 @@
+export { default as ChannelCard } from './ChannelCard';
+export { default as VideoCard } from './VideoCard';
+export { default as ChannelDetail } from './ChannelDetail';
+export { default as Feed } from './Feed';
+export { default as Loader } from './Loader';
+export { default as NavBar } from './NavBar';
+export { default as SearchBar } from './SearchBar';
+export { default as VideoDetails } from './VideoDetails';
+export { default as SearchFeed } from './SearchFeed';
+export { default as Videos } from './Videos';
+export { default as Sidebar } from './Sidebar';
+// ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed 
